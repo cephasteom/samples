fb854c7a13351461d715172aa13662e22065bf46
Peter Thomas <petethomasmusique@gmail.com>
2025-12-07 22:21:39 +0000

    compile list of samples

--- samplesToJSON.js
+++ samplesToJSON.js
@@ -27,7 +27,7 @@ const result = compile(tree.children)
     }), {})
 
 const json = JSON.stringify({
-    _base: 'https://raw.githubusercontent.com/cephasteom/samples/',
+    _base: 'https://raw.githubusercontent.com/cephasteom/samples/main/',
     ...result
 });
 
